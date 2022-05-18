dc817021afd73560384a741ca3fd69915fd92f6b
Hanuvekpucaprvi <86794506+Hanuvekpucaprvi@users.noreply.github.com>
2022-05-19 00:16:18 +0200

    Add files via upload

---
+++ IGRA/js/scenes/boot.js
@@ -0,0 +1,16 @@
+class Boot extends Phaser.Scene {
+
+    constructor() {
+        super({ key: 'Boot', active: true });
+
+    }
+
+    init() {
+    }
+    preload() {
+    }
+    create() {
+        this.scene.start('Preload');
+    }
+
+}
---
+++ IGRA/js/scenes/menu.js
@@ -0,0 +1,16 @@
+class Menu extends Phaser.Scene {
+
+    constructor() {
+        super({ key: 'Menu', active: false });
+
+    }
+
+    init() {
+    }
+    preload() {
+    }
+    create() {
+        console.log(`hi`);
+    }
+
+}
